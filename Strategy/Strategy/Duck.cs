36d46061344e3622854cca3cabbfb47041ff65b6
whizkid <kaustubhd06@gmail.com>
2017-12-25 21:15:44 -0800

    Added class diagram for strategy pattern

---
+++ Strategy/Strategy/Duck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Duck
+{
+    IFlyBehavior flyBehavior_;
+    IQuackBehavior quackBehavior_;
+    IDisplayBehavior displayBehavior_;
+
+    public Duck(
+        FlyBehavior flyBehavior,
+        QuackBehavior quackBehavior,
+        DisplayBehavior displayBehavior) {
+
+        this.flyBehavior_ = flyBehavior;
+        this.quackBehavior_ = quackBehavior;
+        this.displayBehavior_ = displayBehavior;
+    }
+}
